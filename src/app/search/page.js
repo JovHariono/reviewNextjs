98eceac87487f93a1c00cf4dc5ddf0a16fde4d6c
email <myemail@mail.com>
2023-12-07 17:18:51 +0700

    Review Next

---
+++ src/app/search/page.js
@@ -0,0 +1,12 @@
+const Page = () => {
+    return ( 
+        <>
+            <h1>
+            Search Page
+            </h1>
+            <p>Ini adalah Search Page</p>
+        </>
+     );
+}
+ 
+export default Page;
---
+++ src/components/Search/index.js
@@ -0,0 +1,9 @@
+const Search = () => {
+    return ( 
+        <>
+        <h1>Anime Lsisting Component</h1>
+        </>
+     );
+}
+ 
+export default Search;
